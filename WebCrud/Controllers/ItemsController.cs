826e764d051183777e5c716a77ffaddc5215fe2a
rpatel26 <rinks00123@gmail.com>
2026-02-05 10:42:56 -0500

    Add project files.

---
+++ WebCrud/Controllers/ItemsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCrud.Models;
+
+namespace WebCrud.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var items = ItemRepository.GetAll();
+            return View(items);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                ItemRepository.Add(item);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var item = ItemRepository.Get(id);
+            if (item == null) return NotFound();
+            return View(item);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                ItemRepository.Update(item);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(item);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var item = ItemRepository.Get(id);
+            if (item == null) return NotFound();
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            ItemRepository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WebCrud/Models/ItemRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebCrud.Models
+{
+    public static class ItemRepository
+    {
+        private static List<Item> items = new List<Item>();
+        private static int nextId = 1;
+
+        public static List<Item> GetAll() => items;
+
+        public static Item Get(int id) => items.FirstOrDefault(i => i.Id == id);
+
+        public static void Add(Item item)
+        {
+            item.Id = nextId++;
+            items.Add(item);
+        }
+
+        public static void Update(Item item)
+        {
+            var existing = Get(item.Id);
+            if (existing != null)
+            {
+                existing.Name = item.Name;
+                existing.Description = item.Description;
+            }
+        }
+
+        public static void Delete(int id)
+        {
+            var item = Get(id);
+            if (item != null)
+                items.Remove(item);
+        }
+    }
+}
